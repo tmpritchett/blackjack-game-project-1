1e09b56b2408318630e37b2dc424a74f1f0df547
tmpritchett <tanyampritchett@gmail.com>
2018-02-08 04:42:54 -0500

    another commit

--- blackjack.js
+++ blackjack.js
@@ -1,35 +1,65 @@
-let deck = [];
+// let deck = [];
 
-let suits = ['diamonds', 'spades', 'clubs', 'hearts'];
-let values = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+// let suits = ['diamonds', 'spades', 'clubs', 'hearts'];
+// let values = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
 
-function makeDeck() {
+// function makeDeck() {
   
-    for(let x=0; x < suits.length; x++){
-    for(let y=0; y < values.length; y++){
-    let card =
-    { value: values[y], suit: suits[x]
-     };
+//     for(let x=0; x < suits.length; x++){
+//     for(let y=0; y < values.length; y++){
+//     let card =
+//     { value: values[y], suit: suits[x]
+//      };
     
-     } 
-    let royalCard = parseInt(values[y]);
+//      } 
+//     let royalCard = parseInt(values[y]);
     
-      if(values[y] === 'J' || 'Q' ||'K'){
-      let royalCard  = 10;
+//       if(values[y] === 'J' || 'Q' ||'K'){
+//       let royalCard  = 10;
       
-      if(values[y]=== 'A')
-      let royalCard = 11;
+//       if(values[y]=== 'A')
+//       let royalCard = 11;
 
-    let card =
-    { value: values[y], suit: suits[x], royalCard: royalCard[y]
-     };
-     deck.push(card)
-        }
-    }
-    function shuffle () 
+//     let card =
+//     { value: values[y], suit: suits[x], royalCard: royalCard[y]
+//      };
+//      deck.push(card)
+//         }
+//     }
+//   }
+  
+function shuffle()
+{
+    // for 1000 turns
+    // switch the values of two random cards
+    for (var i = 0; i < 1000; i++)
     {
-      
+        var location1 = Math.floor((Math.random() * deck.length));
+        var location2 = Math.floor((Math.random() * deck.length));
+        var tmp = deck[location1];
+
+        deck[location1] = deck[location2];
+        deck[location2] = tmp;
     }
-  
-  }
+}
+
+function renderDeck()
+{
+	for(var i = 0; i < deck.length; i++)
+	{
+		var card = document.createElement("div");
+		var value = document.createElement("div");
+		var suit = document.createElement("div");
+		card.className = "card";
+		value.className = "value";
+		suit.className = "suit " + deck[i].Suit;
+
+		value.innerHTML = deck[i].Value;
+		card.appendChild(value);
+		card.appendChild(suit);
+
+		document.getElementById("deck").appendChild(card);
+	}
+}
+
